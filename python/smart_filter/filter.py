38498ccf100f4294b4132f06a7c476782ba0117e
GermainZ <germanosz@gmail.com>
2013-06-15 22:31:33 +0300

    Directly check if key exists instead of relying on errors

--- python/smart_filter/filter.py
+++ python/smart_filter/filter.py
@@ -21,9 +21,7 @@ def new_msg(word, word_eol, userdata):
     user = xchat.strip(word[0])
     # If the user logged in before we did (which means the Join part of
     # filter_msg didn't take effect), add him to the dict.
-    try:
-        last_seen[user]
-    except KeyError:
+    if user not in last_seen:
         last_seen[user]= [time(), 1]
     # If the user has never spoken before, let us know when he logged in.
     if last_seen[user][1] == 0:
@@ -40,25 +38,22 @@ def filter_msg(word, word_eol, userdata):
     user = xchat.strip(word[0])
     # If the user just joined, add him to the dict and mark him as such
     if userdata == 'Join':
-        try:
-            last_seen[user]
-        except KeyError:
+        if user not in last_seen:
             last_seen[user] = [time(), 0]
             return xchat.EAT_XCHAT
     # If the user changed his nick, check if we've been tracking him before
     # and transfer his stats if so. Otherwise, add him to the dict.
     elif userdata == 'Nick':
         user = xchat.strip(word[1])
-        try:
-            last_seen[user] = last_seen[xchat.strip(word[0])]
-            del last_seen[xchat.strip(word[0])]
-        except KeyError:
+        old = xchat.strip(word[0])
+        if old in last_seen:
+            last_seen[user] = last_seen[old]
+            del last_seen[old]
+        else:
             last_seen[user] = [time(), 0]
     # If the user logged in before we did (no entry of him yet), don't display
     # his part messages
-    try:
-        last_seen[user]
-    except KeyError:
+    if user not in last_seen:
         return xchat.EAT_XCHAT
     # If the user has never spoken, or has spoken too long ago, eat his part
     # or join messages.
