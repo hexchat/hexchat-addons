70904d27cd620e5d1fb162c34893079447286b8b
GermainZ <germanosz@gmail.com>
2013-06-29 22:25:54 +0300

    Handle actions as well

--- python/smart_filter/filter.py
+++ python/smart_filter/filter.py
@@ -13,10 +13,10 @@ user_timeout = 600 # If the user hasn't spoken for this amount of seconds, his
 
 def new_msg(word, word_eol, userdata):
     """Handles normal messages.
-    
+
     Unless this is the first user's message since he joined, the message will
     not be altered. Otherwise, a '(logged in Xs ago)' message will be appended.
-    
+
     """
     user = xchat.strip(word[0])
     # If the user logged in before we did (which means the Join part of
@@ -26,8 +26,20 @@ def new_msg(word, word_eol, userdata):
     # If the user has never spoken before, let us know when he logged in.
     if last_seen[user][1] == 0:
         time_diff = time() - last_seen[user][0]
-        xchat.prnt("%s\t\017%s \00307(logged in %ss ago)" % (word[0], word[1],
-                                                          int(time_diff)))
+        # Bold the username and color the text if it's a hilight
+        if "Hilight" in userdata:
+            xchat.prnt("word: " + '_'.join(word))
+            s_user = "\002" + word[0]
+            s_msg = "\017\00319" + word[1]
+        else:
+            s_user = word[0]
+            s_msg = "\017" + word[1]
+        if "Action" in userdata:
+            s_user = "\00319*\t%s " % s_user
+        else:
+            s_user += '\t'
+        xchat.prnt("%s%s \00307(logged in %ss ago)" % (s_user, s_msg,
+                                                         int(time_diff)))
         last_seen[user]= [time(), 1]
         return xchat.EAT_XCHAT
     else:
@@ -37,13 +49,13 @@ def filter_msg(word, word_eol, userdata):
     """Filters join and part messages"""
     user = xchat.strip(word[0])
     # If the user just joined, add him to the dict and mark him as such
-    if userdata == 'Join':
+    if 'Join' in userdata:
         if user not in last_seen:
             last_seen[user] = [time(), 0]
             return xchat.EAT_XCHAT
     # If the user changed his nick, check if we've been tracking him before
     # and transfer his stats if so. Otherwise, add him to the dict.
-    elif userdata == 'Nick':
+    elif 'Nick' in userdata:
         user = xchat.strip(word[1])
         old = xchat.strip(word[0])
         if old in last_seen:
@@ -61,12 +73,14 @@ def filter_msg(word, word_eol, userdata):
         return xchat.EAT_XCHAT
 
 
-xchat.hook_print('Channel Message', new_msg)
-xchat.hook_print('Channel Msg Hilight', new_msg)
+xchat.hook_print('Channel Message', new_msg, ())
+xchat.hook_print('Channel Msg Hilight', new_msg, "Hilight")
+xchat.hook_print('Channel Action', new_msg, "Action")
+xchat.hook_print('Channel Action Hilight', new_msg, ("Hilight", "Action"))
 xchat.hook_print('Join', filter_msg, 'Join')
 xchat.hook_print('Change Nick', filter_msg, 'Nick')
-xchat.hook_print('Part', filter_msg)
-xchat.hook_print('Part with Reason', filter_msg)
-xchat.hook_print('Quit', filter_msg)
+xchat.hook_print('Part', filter_msg, ())
+xchat.hook_print('Part with Reason', filter_msg, ())
+xchat.hook_print('Quit', filter_msg, ())
 
-print "\00304", __module_name__, "successfully loaded.\003"
+print("\00304", __module_name__, "successfully loaded.\003")
