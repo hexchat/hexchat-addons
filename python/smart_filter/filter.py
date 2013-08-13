66c78125b2cc553b6c288279e30c7b426c3b1b64
GermainZ <germanosz@gmail.com>
2013-08-14 02:17:47 +0300

    Use emit_print instead of trying to replicate all the colors again

--- python/smart_filter/filter.py
+++ python/smart_filter/filter.py
@@ -2,7 +2,7 @@ __module_name__ = 'Filter'
 __module_version__ = '1.0'
 __module_description__ = 'Filters join/part messages'
 
-import xchat
+import hexchat
 from time import time
 
 last_seen = {} # For each entry: the key is the user's nickname, the entry
@@ -10,15 +10,20 @@ last_seen = {} # For each entry: the key is the user's nickname, the entry
                #            element 1: 0 if the user never spoke, 1 otherwise
 user_timeout = 600 # If the user hasn't spoken for this amount of seconds, his
                    # join/part messages won't be shown
+halt = False
 
-def new_msg(word, word_eol, userdata):
+
+def new_msg(word, word_eol, event, attrs):
     """Handles normal messages.
 
     Unless this is the first user's message since he joined, the message will
     not be altered. Otherwise, a '(logged in Xs ago)' message will be appended.
 
     """
-    user = xchat.strip(word[0])
+    global halt
+    if halt is True:
+        return
+    user = hexchat.strip(word[0])
     # If the user logged in before we did (which means the Join part of
     # filter_msg didn't take effect), add him to the dict.
     if user not in last_seen:
@@ -26,37 +31,30 @@ def new_msg(word, word_eol, userdata):
     # If the user has never spoken before, let us know when he logged in.
     if last_seen[user][1] == 0:
         time_diff = time() - last_seen[user][0]
-        # Bold the username and color the text if it's a hilight
-        if "Hilight" in userdata:
-            s_user = "\002" + word[0]
-            s_msg = "\017\00319" + word[1]
-        else:
-            s_user = word[0]
-            s_msg = "\017" + word[1]
-        if "Action" in userdata:
-            s_user = "\00319*\t%s " % s_user
-        else:
-            s_user += '\t'
-        xchat.prnt("%s%s \00307(logged in %ss ago)" % (s_user, s_msg,
-                                                         int(time_diff)))
+        word[1] += " \00307(logged in %ss ago)" % int(time_diff)
+        halt = True
+        hexchat.emit_print(event, *word)
+        halt = False
         last_seen[user]= [time(), 1]
-        return xchat.EAT_XCHAT
+        return hexchat.EAT_ALL
     else:
         last_seen[user]= [time(), 1]
 
-def filter_msg(word, word_eol, userdata):
+def filter_msg(word, word_eol, event, attrs):
     """Filters join and part messages"""
-    user = xchat.strip(word[0])
+    user = hexchat.strip(word[0])
     # If the user just joined, add him to the dict and mark him as such
-    if 'Join' in userdata:
+    #if 'Join' in userdata:
+    if event == "Join":
         if user not in last_seen:
             last_seen[user] = [time(), 0]
-            return xchat.EAT_XCHAT
+            return hexchat.EAT_ALL
     # If the user changed his nick, check if we've been tracking him before
     # and transfer his stats if so. Otherwise, add him to the dict.
-    elif 'Nick' in userdata:
-        user = xchat.strip(word[1])
-        old = xchat.strip(word[0])
+    #elif 'Nick' in userdata:
+    elif event == "Change Nick":
+        user = hexchat.strip(word[1])
+        old = hexchat.strip(word[0])
         if old in last_seen:
             last_seen[user] = last_seen[old]
             del last_seen[old]
@@ -65,21 +63,21 @@ def filter_msg(word, word_eol, userdata):
     # If the user logged in before we did (no entry of him yet), don't display
     # his part messages
     if user not in last_seen:
-        return xchat.EAT_XCHAT
+        return hexchat.EAT_ALL
     # If the user has never spoken, or has spoken too long ago, eat his part
     # or join messages.
     if last_seen[user][1] == 0 or last_seen[user][0] + user_timeout < time():
-        return xchat.EAT_XCHAT
+        return hexchat.EAT_ALL
 
 
-xchat.hook_print('Channel Message', new_msg, ())
-xchat.hook_print('Channel Msg Hilight', new_msg, "Hilight")
-xchat.hook_print('Channel Action', new_msg, "Action")
-xchat.hook_print('Channel Action Hilight', new_msg, ("Hilight", "Action"))
-xchat.hook_print('Join', filter_msg, 'Join')
-xchat.hook_print('Change Nick', filter_msg, 'Nick')
-xchat.hook_print('Part', filter_msg, ())
-xchat.hook_print('Part with Reason', filter_msg, ())
-xchat.hook_print('Quit', filter_msg, ())
+hooks_new = ["Your Message", "Channel Message", "Channel Msg Hilight",
+             "Your Action", "Channel Action", "Channel Action Hilight"]
+hooks_filter = ["Join", "Change Nick", "Part", "Part with Reason", "Quit"]
+# hook_print_attrs is used for compatibility with my other scripts,
+# since priorities are hook specific
+for hook in hooks_new:
+    hexchat.hook_print_attrs(hook, new_msg, hook, hexchat.PRI_HIGH)
+for hook in hooks_filter:
+    hexchat.hook_print_attrs(hook, filter_msg, hook, hexchat.PRI_HIGH)
 
 print("\00304", __module_name__, "successfully loaded.\003")
