f868f95fe2a079e2b0320c92abec9216aa946ad0
djancak <djancak@gmail.com>
2014-06-12 00:59:45 -0500

    Update slap.py

--- python/slap/slap.py
+++ python/slap/slap.py
@@ -1,15 +1,15 @@
 __module_name__ = "Hexchat Slap Plugin"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Slap command"
 __author__ = "Douglas Brunal (AKA) Frankity"
 
-import xchat as XC
+import hexchat as XC
 
 def slaps(word, word_eol, userdata):
     try:
-        XC.command('me ' + 'Slap ' + '\002'+word[1]+'\002' + ' in da face with a large trout')
+        XC.command('me ' + 'slaps ' + '\002'+word[1]+'\002' + ' in da face with a large trout')
     except:
-        print 'error '
+        print('error ')
 
 XC.hook_command("slap",slaps)
 XC.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
