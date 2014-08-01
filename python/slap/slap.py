559c771ea492d182ecd84dd0d39a967a6c4ec1ba
TingPing <TingPing@users.noreply.github.com>
2014-08-01 01:53:39 -0400

    Improve slap.py
    I am not sure why but many new scripters base their first one off of this.
    So in an attempt to stop terrible habits I cleaned it up.
    
    - Be compat with both Python 2 and Python 3
    - Proper error handling
    - Add help info
    - Proper string formatting
    - Return EAT_ALL
    - Consistent syntax

--- python/slap/slap.py
+++ python/slap/slap.py
@@ -1,15 +1,22 @@
-__module_name__ = "Hexchat Slap Plugin"
-__module_version__ = "0.1"
-__module_description__ = "Slap command"
-__author__ = "Douglas Brunal (AKA) Frankity"
+from __future__ import print_function
+import hexchat
 
-import xchat as XC
+__module_name__ = 'Slap'
+__module_version__ = '1.0'
+__module_description__ = 'Slaps specified users'
+__author__ = 'Douglas Brunal (AKA) Frankity'
 
-def slaps(word, word_eol, userdata):
-    try:
-        XC.command('me ' + 'Slap ' + '\002'+word[1]+'\002' + ' in da face with a large trout')
-    except:
-        print 'error '
+def slap_cb(word, word_eol, userdata):
+  if len(word) > 1:
+    hexchat.command('me slaps {} in da face with a large trout'.format(word[1]))
+  else:
+    hexchat.command('help slap')
 
-XC.hook_command("slap",slaps)
-XC.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
+  return hexchat.EAT_ALL
+
+def unload_cb(userdata):
+  print(__module_name__, 'version', __module_version__, 'unloaded.')
+
+hexchat.hook_command('slap', slap_cb, help='SLAP <nick>')
+hexchat.hook_unload(unload_cb)
+print(__module_name__, 'version', __module_version__, 'loaded.')
