c668444590dc255e85f3bb45d7340c8d0e52c9bd
Nikersify <nikersifyyt@gmail.com>
2014-09-01 16:42:41 +0200

    added readme, disabled threads because reasons

---
+++ python/twitch_enhancements/readme.md
@@ -0,0 +1,29 @@
+Twitch Enhancements Plugin
+===
+
+Passively outputs jtv messages (like bans, sending messages too quickly etc.) to a current taband provides stream information.
+
+Requirements
+------------
+
+* Hexchat (http://hexchat.org/)
+* Hexchat [Python Module](http://i.imgur.com/ykfQaUY.png)
+* little bit of brain
+
+Installation
+------------
+
+Right click > Save link as... [me](https://raw.githubusercontent.com/hexchat/hexchat-addons/master/python/twitch_enhancements/twitch_enchancements.py) to a *.py file and put it in your `config/addons` folder. If you chose "portable mode" during the installation process it's in your installation folder, if not it's most likely `%appdata%/HexChat/addons`.
+
+Commands
+--------
+
+No arguments, commands refer to currently opened tab.
+
+* `/STREAM` - Checks if stream is online and if so it displays its name, title and url
+* `/UPTIME` - Checks for how long user has been streaming
+
+Whatever
+--------
+
+If you find a bug you're not alone.
--- python/twitch_enhancements/twitch_enhancements.py
+++ python/twitch_enhancements/twitch_enhancements.py
@@ -6,22 +6,25 @@ import sys
 
 from datetime import datetime, timedelta, date, time
 
-THREADS_SUPPORTED = True
+#SET THIS TO TRUE IF YOU WANT THE BUGGY THREADS
+THREADS_SUPPORTED = False
+
 if sys.version_info[0] == 2:
 	import urllib
 	if sys.platform == 'win32':
+		format("Using this plugin is not recommended your current platform.")
 		THREADS_SUPPORTED = False
 else:
 	import urllib.request as urllib
 
 import hexchat
 
-__module_name__ = "Twitch enchancements"
+__module_name__ = "Twitch enhancements"
 __module_version__ = "0.2"
-__module_description__ = "Prints jtv messages (timeouts n' stuff)"
+__module_description__ = "Prints jtv messages, provides stream information"
 
 liveChannels = []
-firstRun = 1
+firstRun = True
 
 def checkmessage_cb(word, word_eol, userdata):
 	string = ' '.join(word[0:3])
@@ -35,7 +38,8 @@ def checkmessage_cb(word, word_eol, userdata):
 
 def stream_cb(word, word_eol, userdata):
 	if THREADS_SUPPORTED:
-		threading.Thread(target = stream).start()
+		streamThread = threading.Thread(target = stream)
+		streamThread.start()
 	else:
 		stream()
 
@@ -43,30 +47,30 @@ def stream():
 	CHANNEL = hexchat.get_info("channel")
 
 	if (hexchat.get_info("host") != "irc.twitch.tv"):
-		print("/STREAM works only in irc.twitch.tv chats")
-		return hexchat.EAT_ALL
+		hexchat.prnt("/STREAM works only in irc.twitch.tv chats")
+		return
 	
 	obj = loadJSON('https://api.twitch.tv/kraken/streams/' + CHANNEL.strip('#'))
 	
 	if (obj["stream"] == None):
 		format(CHANNEL.title() + " is not live on twitch.tv.", special=1)
 	else:
-		format(CHANNEL.title() + " is streaming for " + str(obj["stream"]["viewers"]) + " viewers on " + obj["stream"]["channel"]["url"], special=1)
-	
-	return hexchat.EAT_ALL
+		format(CHANNEL.title() + " is streaming for " + str(obj["stream"]["viewers"]) + " viewers on " + obj["stream"]["channel"]["url"], special=1)	
 
 def checkStreams_cb(userdata):
 	if THREADS_SUPPORTED:
-		threading.Thread(target = checkStreams).start()
+		checkStreamsThread = threading.Thread(target = checkStreams)
+		checkStreamsThread.start()
 	else:
 		checkStreams()
+
 
 def checkStreams():
 	global firstRun
 	if(firstRun):
 		hexchat.unhook(timerHook)
 		hexchat.hook_timer(300000, checkStreams_cb)
-		firstRun = 0
+		firstRun = False
 
 	channels = hexchat.get_list("channels")
 	realChannels = []
@@ -83,45 +87,51 @@ def checkStreams():
 			liveChannels.append(channel)
 			format(channel.title() + " is live!")
 
-	return 1
-
 def uptime_cb(word, word_eol, userdata):
 	if THREADS_SUPPORTED:
-		threading.Thread(target = uptime).start()
+		uptimeThread = threading.Thread(target = uptime)
+		uptimeThread.start()
 	else:
 		uptime()
+
 
 def uptime():
 	user = hexchat.get_info("channel").strip('#')
 	url = 'https://api.twitch.tv/kraken/channels/' + user + '/videos?limit=1&broadcasts=true'
-	
-	latestbroadcast = json.loads(urllib.urlopen(url).readall().decode('utf-8'))['videos'][0]['_id']
-	secondurl = 'https://api.twitch.tv/kraken/videos/' + latestbroadcast
-	starttimestring = json.loads(urllib.urlopen(secondurl).readall().decode('utf-8'))['recorded_at']
 
-	timeFormat = "%Y-%m-%dT%H:%M:%SZ"
-	startdate = datetime.strptime(starttimestring, timeFormat)
-	currentdate = datetime.utcnow()
-	combineddate = currentdate - startdate - timedelta(microseconds=currentdate.microsecond)
+	try:
+		obj = loadJSON('https://api.twitch.tv/kraken/streams/' + user)
+		if(obj["stream"] == None):
+			raise Exception
+		
+		latestbroadcast = loadJSON(url)['videos'][0]['_id']
+		secondurl = 'https://api.twitch.tv/kraken/videos/' + latestbroadcast
+		starttimestring = loadJSON(secondurl)['recorded_at']
 
-	format(user.title() + " has been streaming for " + str(combineddate))
+		timeFormat = "%Y-%m-%dT%H:%M:%SZ"
+		startdate = datetime.strptime(starttimestring, timeFormat)
+		currentdate = datetime.utcnow()
+		combineddate = currentdate - startdate - timedelta(microseconds=currentdate.microsecond)		
+		format(user.title() + " has been streaming for " + str(combineddate))
+	except Exception:
+		format(user.title() + " is not streaming.")
 
 def unload_cb(userdata):
-	print("\00304", __module_name__, __module_version__, "successfully unloaded.\003")
+	hexchat.prnt("\00304" + __module_name__ + __module_version__ + "successfully unloaded.\003")
 
 def format(string, special=0):
 	if(special):
 		string = string.replace(string[:1],'')
 	string = '\002\035' + string
-	print(string)
+	hexchat.prnt(string)
 
 def loadJSON(url):
 	try:
-		with urllib.urlopen(url) as data:
+		with urllib.urlopen(url, timeout=3) as data:
 			obj = json.loads(data.readall().decode('utf-8'))
 			return obj
 	except Exception:
-		return json.dumps()
+		return json.dumps(None)
 
 hexchat.hook_server('PRIVMSG', checkmessage_cb)
 hexchat.hook_command('STREAM', stream_cb, help ="/STREAM Use in twitch.tv chats to check if the stream is online.")
@@ -132,4 +142,4 @@ hexchat.hook_unload(unload_cb)
 timerHook = hexchat.hook_timer(10000, checkStreams_cb)
 
 
-print("\00304", __module_name__, __module_version__, "successfully loaded.\003")
+hexchat.prnt("\00304" + __module_name__ + __module_version__ + "successfully loaded.\003")
