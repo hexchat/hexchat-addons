0d00e705c711a44c3c071c3d8e6d827e6c6fa1cd
Thor77 <xXThor77Xx@gmail.com>
2015-09-24 17:04:54 +0200

    slap.py: indent with 4 spaces

--- python/slap/slap.py
+++ python/slap/slap.py
@@ -6,16 +6,17 @@ __module_version__ = '1.0'
 __module_description__ = 'Slaps specified users'
 __author__ = 'Douglas Brunal (AKA) Frankity'
 
-def slap_cb(word, word_eol, userdata):
-  if len(word) > 1:
-    hexchat.command('me slaps {} in da face with a large trout'.format(word[1]))
-  else:
-    hexchat.command('help slap')
 
-  return hexchat.EAT_ALL
+def slap_cb(word, word_eol, userdata):
+    if len(word) > 1:
+        hexchat.command('me slaps {} in da face with a large trout'.format(word[1]))
+    else:
+        hexchat.command('help slap')
+    return hexchat.EAT_ALL
+
 
 def unload_cb(userdata):
-  print(__module_name__, 'version', __module_version__, 'unloaded.')
+    print(__module_name__, 'version', __module_version__, 'unloaded.')
 
 hexchat.hook_command('slap', slap_cb, help='SLAP <nick>')
 hexchat.hook_unload(unload_cb)
