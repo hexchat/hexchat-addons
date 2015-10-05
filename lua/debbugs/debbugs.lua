f7152f10bd7864a92040ab6f2d2c70009ea2dd85
mniip <mniip@mniip.com>
2015-10-05 21:25:03 +0300

    Rewrite debbugs.lua for use with the new lua plugin

--- lua/debbugs/debbugs.lua
+++ lua/debbugs/debbugs.lua
@@ -1,34 +1,19 @@
---
--- $Id$
---
--- requires xchat cvs version from 2007-05-20 (or later) 
---
-function xchat_register()
-    return "debbugs.lua", "Show debian bugs on right click", "0.1"
+hexchat.register("debbugs.lua", "0.1", "Show debian bugs on right click")
+
+local logo = "/usr/share/pixmaps/debian-logo-16x16.png"
+
+local function unload()
+	hexchat.command("MENU DEL \"$CHAN/Debian Bug #\"")
 end
 
-function xchat_init()
-    xchat.hook_command("DEBBUG", "cmd_debbug")
-    local logo = "/usr/share/pixmaps/debian-logo-16x16.png"
-    xchat.commandf("MENU -i%s ADD \"$CHAN/Debian Bug #\" \"DEBBUG %%s\"", logo)
+local function cmd_debbug(word, eol, data)
+	local bug = word[2]:match"#(%d+)$"
+	if bug then
+		hexchat.command("URL http://bugs.debian.org/" .. tonumber(bug))
+	end
+	return hexchat.EAT_HEXCHAT
 end
 
-function xchat_unload()
-    xchat.command("MENU DEL \"$CHAN/Debian Bug #\"")
-end
-
-function cmd_debbug(word, eol, data)
-    local bug = word[2]
-    if string.find(bug, "#", 1, true) == 1 then
-        bug = string.sub(bug, 2)
-    end
-    if string.find(bug, "^%d+$") then
-        xchat.commandf(
-            "EXEC /usr/bin/x-www-browser http://bugs.debian.org/%d", 
-                tonumber(bug)
-        )
-    else
-        xchat.printf("DEBBUG: bug number '%s' contains non digits...", bug)
-    end
-    return xchat.EAT_XCHAT
-end
+hexchat.hook_unload(unload)
+hexchat.hook_command("DEBBUG", cmd_debbug)
+hexchat.command("MENU -i" .. logo .. " ADD \"$CHAN/Debian Bug #\" \"DEBBUG %s\"")
