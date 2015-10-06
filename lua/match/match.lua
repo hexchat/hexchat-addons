797450b0bb9b039c5d0ad72faad3edab7a0c5eea
mniip <mniip@mniip.com>
2015-10-06 20:43:29 +0300

    Rewrite match.lua for use with the new lua plugin

--- lua/match/match.lua
+++ lua/match/match.lua
@@ -1,71 +1,53 @@
---
--- $Id$
--- $Revision$
--- $Date$
---
 local name = "match.lua"
 local desc = "do pattern matching on user@host, nick, ..."
 local version = "0.2"
 
-function xchat_register()
-    return name, desc, version
+hexchat.register(name, version, desc)
+
+local function irc2lua(str)
+	return str:gsub("%p", function(p)
+		if p == "?" then
+			return "."
+		elseif p == "*" then
+			return ".*"
+		else
+			return "%" .. p
+		end
+	end)
 end
 
-function xchat_init()
-    local help = "MATCH: Usage: MATCH [-l] PATTERN"
-    -- -l: pattern is lua pattern, not default irc pattern
-    xchat.hook_command("MATCH", "cmd_match", xchat.PRI_NORM, help)
+local function cmd_match(word, eol, data)
+	local pattern
+	local lua
+	if word[2] == "-l" then
+		pattern = word[3]
+		lua = true
+	else
+		pattern = word[2]
+		lua = false
+	end
+
+	local matches = {}
+	for user in hexchat.iterate"users" do
+		local pat = pattern
+		if not lua then
+			pat = irc2lua(pat)
+		end
+		if user.nick:find(pat) or (user.host or ""):find(pat) then
+			table.insert(matches, {user.nick, user.host or ""})
+		end
+	end
+	if #matches == 0 then
+		hexchat.print("MATCH: no matches for '" .. pattern .. "'")
+	else
+		hexchat.print("------------ matches for '" .. pattern .. "' ---------------")
+		for _, m in ipairs(matches) do
+			hexchat.print(("%-15s %s!%s"):format(m[1], m[1], m[2]))
+		end
+	end
+	return hexchat.EAT_ALL
 end
 
-function irc2lua(str)
-    str = string.gsub(str, "%?", "\0")
-    str = string.gsub(str, "%%", "%%%%")
-    str = string.gsub(str, "%.", "%%.")
-    str = string.gsub(str, "%*", ".*")
-    str = string.gsub(str, "%z", ".")
-    str = string.gsub(str, "([^%w%%.*])", "%%%1")
-    return str
-end
+local help = "MATCH: Usage: MATCH [-l] PATTERN"
 
-function cmd_match(word, eol, data)
-    local pattern, opattern
-    local matches = {}
-    if string.find(word[2], "^%-") and string.find(word[2], "l") then
-        opattern = word[3]
-        pattern  = word[3]
-    else
-        opattern = word[2]
-        pattern  = irc2lua(word[2])
-    end
-
-    local users   = xchat.list_get("users")
-    if users == nil then
-        xchat.print("MATCH: not on a channel?")
-        return xchat.EAT_XCHAT
-    end
-
-    table.foreach(users,
-        function(i, list)
-            if string.find(list.nick, pattern) 
-                or
-               string.find(list.host, pattern)
-            then
-                table.insert(matches, { list.nick, list.host })
-            end
-        end
-    )
-
-    if table.getn(matches) == 0 then
-        xchat.printf("MATCH: no matches for '%s'", opattern)
-    else
-        xchat.printf("------------ matches for '%s' ---------------", opattern)
-        table.foreach(matches,
-            function(i, list)
-                xchat.printf("%-15s %s!%s", list[1], list[1], list[2])
-            end
-        )
-    end
-    return xchat.EAT_XCHAT
-end
-
--- END
+hexchat.hook_command("MATCH", cmd_match, help)
