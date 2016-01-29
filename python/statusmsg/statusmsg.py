63e3861b16958fc6c5f70f6f2a26047b7383664a
mniip <mniip@mniip.com>
2016-01-29 08:07:09 +0300

    statusmsg.py: Make it possible to distinguish statusmsg messages from regular ones

---
+++ python/statusmsg/statusmsg.py
@@ -0,0 +1,38 @@
+__module_name__ = "statusmsg"
+__module_version__ = "1.0.0"
+__module_description__ = "Highlight statusmsg (+#channel, @#channel, etc) messages in a distinct way."
+__module_author__ = "mniip"
+
+import hexchat
+
+lastTarget = None
+lastSource = None
+
+def privmsg(w, we, u):
+    global lastTarget, lastSource
+    lastTarget = w[2]
+    lastSource = w[0].split("!", 1)[0].lstrip(":")
+    return hexchat.EAT_NONE
+
+recursion = False
+def msg_event(w, we, event):
+    global recursion
+    if recursion:
+        return hexchat.EAT_NONE
+    source = w[0]
+    target = hexchat.get_info("channel")
+    if hexchat.nickcmp(source, lastSource) == 0:
+        if hexchat.nickcmp(target, lastTarget[-len(target):]) == 0:
+            status = lastTarget[:-len(target)]
+            if len(status):
+                if not any(c.isalpha() or c.isdigit() for c in status):
+                    w[2] = "[" + status + "]" + w[2]
+                    recursion = True
+                    hexchat.emit_print(event, *w)
+                    recursion = False
+                    return hexchat.EAT_ALL
+    return hexchat.EAT_NONE
+
+hexchat.hook_server("PRIVMSG", privmsg)
+for e in ("Channel Message", "Channel Msg Hilight", "Channel Action", "Channel Action Hilight"):
+    hexchat.hook_print(e, msg_event, e)
