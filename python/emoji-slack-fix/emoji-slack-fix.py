0bc360178644f6c502013e7af0f237b975959dde
Eitan Adler <lists@eitanadler.com>
2018-07-08 09:24:34 -0700

    Add slack-emoji-fix
    From http://www.artificialworlds.net/blog/2018/02/16/fixing-slack-emojis-in-hexchat/

---
+++ python/emoji-slack-fix/emoji-slack-fix.py
@@ -0,0 +1,34 @@
+# Replace all the horrible :slightly_smiling_face: rubbish that Slack inserts
+# into horrible Unicode emoji symbols.
+# Author: Andy Balaam
+# License: CC0 https://creativecommons.org/publicdomain/zero/1.0/
+
+# Requires https://pypi.python.org/pypi/emoji - I used 0.4.5
+# I manually copied the emoji dir into:
+# /home/andrebal/.local/lib/python2.7/site-packages
+import emoji
+import hexchat
+
+__module_name__ = "slack-emojis"
+__module_version__ = "1.0"
+__module_description__ = "Translate emojis from Slack with colons into emojis"
+
+print "Loading slack-emojis"
+chmsg = "Channel Message"
+prmsg = "Private Message to Dialog"
+
+def preprint(words, word_eol, userdata):
+    txt = word_eol[1]
+    replaced = emoji.emojize(txt, use_aliases=True)
+    if replaced != txt:
+        hexchat.emit_print(
+            userdata["msgtype"],
+            words[0],
+            replaced.encode('utf-8'),
+        )
+        return hexchat.EAT_HEXCHAT
+    else:
+        return hexchat.EAT_NONE
+
+hexchat.hook_print(chmsg, preprint, {"msgtype": chmsg})
+hexchat.hook_print(prmsg, preprint, {"msgtype": prmsg})
